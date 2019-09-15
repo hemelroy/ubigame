ef59d5d3f6a0427c217804209a41d4eb1da10e8d
Ayman Shaikh <aymanshaikh@gmail.com>
2019-09-14 21:58:18 -0400

    new hope

---
+++ UbiGame/Source/Game/Components/PlayerMovementComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "GameEngine\EntitySystem\Component.h"
+
+namespace Game
+{
+	class PlayerMovementComponent : public GameEngine::Component
+	{
+	public:
+		PlayerMovementComponent();
+		~PlayerMovementComponent();
+
+		virtual void Update() override;
+		virtual void OnAddToWorld() override;
+
+	private:
+		int m_lastFaceIndex;
+	};
+}
