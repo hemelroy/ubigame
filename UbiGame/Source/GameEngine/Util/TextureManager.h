a8210ddfae2391ca834f4bc9cab1cd12b81094e7
Hemel Roy <hemelroy@hotmail.com>
2019-09-14 21:39:58 -0400

    First

---
+++ UbiGame/Source/GameEngine/Util/TextureManager.h
@@ -0,0 +1,65 @@
+#pragma once
+#include <vector>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Vector2.hpp>
+
+namespace GameEngine
+{
+	//TODO - if needed, move out of Engine part to some sort of loader on the game side of things
+	namespace eTexture
+	{
+		enum type
+		{
+			None = -1,
+			Obstacle,
+			Player,
+			Background,
+			Transparent,
+			Count,
+		};
+	}	
+
+	inline const char* GetPath(eTexture::type texture)
+	{
+		switch (texture)
+		{
+		case eTexture::Obstacle: return "Obstacle.png";
+		case eTexture::Player: return "Player.png";
+		case eTexture::Background: return "background.png";
+		case eTexture::Transparent: return "Transparent.png";
+		}
+		return "UnknownTexType";
+	}
+
+	class TextureManager
+	{
+	public:
+		static TextureManager* GetInstance() { if (!sm_instance) sm_instance = new TextureManager(); return sm_instance; }
+		~TextureManager();
+
+		void LoadTextures();
+		void UnLoadTextures();
+
+		sf::Texture* GetTexture(eTexture::type texture) const { return m_textures[(int)texture]; }
+
+	private:
+		TextureManager();
+		static TextureManager* sm_instance;
+
+		sf::Texture* m_textures[eTexture::Count];
+	};
+}
+
+namespace TextureHelper
+{
+	static sf::Vector2f GetTextureTileSize(GameEngine::eTexture::type texture)
+	{
+		switch (texture)
+		{
+		case  GameEngine::eTexture::Player:  return sf::Vector2f(26.f, 31.f);
+		}
+
+		return sf::Vector2f(-1.f, -1.f);
+	}
+}
+
